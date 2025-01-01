353c81152c1af5c176eeac08378120789001024d
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-01 16:52:55 +0530

    Update All Routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import {MoviList, MovieDetails, MovieSearch, PageNotFound} from '../pages/index'
+import { Routes, Route } from 'react-router-dom';
+import {MoviList, MovieDetails, MovieSearch, PageNotFound} from '../pages';
 
 function AllRoutes(){
     return (
         <>
             <Routes>
                 <Route path="/" element={<MoviList />} />
-                <Route path="MovieSearch/" element={<MovieSearch />} />
-                <Route path="Movie/id" element={<MovieDetails />} />
-                <Route path="Movie/upcoming" element={<MoviList />} />
-                <Route path="Movie/top" element={<MoviList />} />
-                <Route path="Movie/popular" element={<MoviList />} />
+                <Route path="search/" element={<MovieSearch />} />
+                <Route path="movie/:id" element={<MovieDetails />} />
+                <Route path="movie/upcoming" element={<MoviList />} />
+                <Route path="movie/top" element={<MoviList />} />
+                <Route path="movie/popular" element={<MoviList />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </>
