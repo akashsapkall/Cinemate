096fbf6f4129bdc94780072ea04aa6cd2a944ae4
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-03 16:53:19 +0530

    tryong to add theme toggler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import logo from '../assets/logo.png';
-// import logo1 from "../assets/logo1.png";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   const [verticalNav, setVearticalNav] = useState(false);
-  const handleClick=()=>{
-    setVearticalNav((vearticalNav)=>!vearticalNav);
-  }
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkmode"))
+  );
+  useEffect(() => {
+    localStorage.setItem("darkmode", JSON.stringify(darkMode));
+
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const handleClick = () => {
+    setVearticalNav((vearticalNav) => !vearticalNav);
+  };
+
   const activeclass =
     "block py-2 px-3 text-white bg-blue-700 rounded dark:border-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500";
 
   const inactiveclass =
-    "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:dark:hover:bg-transparent dark:border-gray-700"
-    
+    "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:dark:hover:bg-transparent dark:border-gray-700";
+
   return (
     <>
       <nav className="bg-white border-b border-slate-300 dark:bg-gray-900">
@@ -24,10 +37,53 @@ export const Header = () => {
           >
             <img src={logo} className="h-8 text-white" alt="Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Cinemate  
+              Cinemate
             </span>
           </NavLink>
+
           <div className="flex md:order-2">
+            <button
+              data-collapse-toggle="navbar-search"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-search"
+              aria-expanded="false"
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              {darkMode ? (
+                <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"
+                />
+              </svg>
+              ) : (
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"
+                  />
+                </svg>
+              )}
+            </button>
             <button
               type="button"
               data-collapse-toggle="navbar-search"
@@ -53,6 +109,7 @@ export const Header = () => {
               </svg>
               <span className="sr-only">Search</span>
             </button>
+
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
@@ -79,6 +136,7 @@ export const Header = () => {
                 placeholder="Search..."
               />
             </div>
+
             <button
               data-collapse-toggle="navbar-search"
               type="button"
@@ -105,8 +163,11 @@ export const Header = () => {
               </svg>
             </button>
           </div>
+
           <div
-            className={`${verticalNav?"":"hidden"} items-center justify-between w-full md:flex md:w-auto md:order-1"
+            className={`${
+              verticalNav ? "" : "hidden"
+            } items-center justify-between w-full md:flex md:w-auto md:order-1"
             id="navbar-search`}
           >
             <div className="relative mt-3 md:hidden">
@@ -138,7 +199,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/"
-                  className={({isActive}) =>
+                  className={({ isActive }) =>
                     isActive ? activeclass : inactiveclass
                   }
                   end
@@ -150,7 +211,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/movie/popular"
-                  className={({isActive}) =>
+                  className={({ isActive }) =>
                     isActive ? activeclass : inactiveclass
                   }
                 >
@@ -160,7 +221,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/movie/top"
-                  className={({isActive}) =>
+                  className={({ isActive }) =>
                     isActive ? activeclass : inactiveclass
                   }
                 >
@@ -170,7 +231,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/movie/upcoming"
-                  className={({isActive}) =>
+                  className={({ isActive }) =>
                     isActive ? activeclass : inactiveclass
                   }
                 >
