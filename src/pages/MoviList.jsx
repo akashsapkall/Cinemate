958c74a4966ce9421c9f9f74f1e1f9949afa4727
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-05 10:25:28 +0530

    update movie list with movie card and api integration

--- src/pages/MoviList.jsx
+++ src/pages/MoviList.jsx
@@ -1,7 +1,27 @@
-export const MoviList=()=>{
-    return (
-        <main>
-            <h1>Movi List</h1>
-        </main>
-    )
-}
+import { useState, useEffect } from "react";
+import { MovieCard } from "../components/MovieCard";
+export const MoviList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchAPI(){
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=db04846aba23641dc5323eaa08c756c8"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchAPI();
+  }, []);
+  return (
+    <main>
+      <section className="w-scren py-7">
+        <div className="mx-auto flex justify-center flex-wrap">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
