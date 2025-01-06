37ba88e737d78a28b7714bf8d3cbf86082518688
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-06 19:10:54 +0530

    update useFetch with proper error handeling and integratig .env

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (path) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `${baseUrl}${path}?api_key=${apiKey}`;
+
+    async function fetchAPI() {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json.results || []);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchAPI();
+  }, [path]);
+
+  return { data, error, loading };
+};
