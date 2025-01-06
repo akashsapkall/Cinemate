7096a33691fbe75314b66dabd1889a207e1cf535
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-06 23:38:35 +0530

    update nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -187,7 +187,7 @@ export const Header = () => {
               />
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              <li onClick={handleClick}>
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
@@ -199,7 +199,7 @@ export const Header = () => {
                   Home
                 </NavLink>
               </li>
-              <li>
+              <li onClick={handleClick}>
                 <NavLink
                   to="/movie/popular"
                   className={({ isActive }) =>
@@ -209,7 +209,7 @@ export const Header = () => {
                   Popular
                 </NavLink>
               </li>
-              <li>
+              <li onClick={handleClick}>
                 <NavLink
                   to="/movie/top"
                   className={({ isActive }) =>
@@ -219,7 +219,7 @@ export const Header = () => {
                   Top-Rated
                 </NavLink>
               </li>
-              <li>
+              <li onClick={handleClick}>
                 <NavLink
                   to="/movie/upcoming"
                   className={({ isActive }) =>
